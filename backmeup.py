c2cd757824c145e0fb26ef480793802c63aac7e1
petermlm <pedromelgueira@gmail.com>
2013-06-23 17:34:57 +0100

    Final code for first deploy

--- backmeup.py
+++ backmeup.py
@@ -7,33 +7,50 @@ import datetime
 
 # Compress to the backup file
 def Compress():
-    # Get list of outer files in outer_files
-    outer_file = GetOuterFiles()
+    # Get list of files and directories to compress. List will have tuples like:
+    # (real directory, object, dir in tar)
+    to_compress = getFilesToCompress()
 
-    # Add Documents directory to list of files
-    # list_of_files = ['~/Documents'] + outer_file
-    list_of_files = ['~/test', 'outer_files'] + outer_file
+    # Add bmu_files to list of files to compress
+    # list_of_files = ['bmu_files'] + to_compress
 
-    # Get name of file
+    # Get name of tar file
     file_name = getFileName()
 
-    # Make tar
-    cmd = 'tar -cf ' + file_name + ' '
-    for i in list_of_files:
-        cmd += i + ' '
-    cmd = cmd.strip()
-
+    # Make tar with bmu_files
+    cmd = 'tar -cf ' + file_name + ' bmu_files'
     os.system(cmd)
+    directory = os.getcwd() + '/' + file_name
 
-# Get outer files from the files outer_files
-def GetOuterFiles():
-    outer_file = open('outer_files')
+    print 'Compressing to ' + directory
+
+    # Add files in to_compress
+    for i in to_compress:
+        print 'Adding file ' + i[0] + '/' + i[1]
+        os.chdir(i[0])
+        cmd = 'tar -r --file=' + directory + ' ' + i[1]
+        os.system(cmd)
+
+# Get files and directories from the file bmu_files
+def getFilesToCompress():
+    to_compress = open('bmu_files')
     list_of_files = []
 
-    for i in outer_file:
-        list_of_files.append(i)
+    for i in to_compress:
+        if i != '\n':
+            if i[0] != '/':
+                app = os.getcwd() + '/'
+            else:
+                app = ''
+            list_of_files.append(makeTuple(app + i))
 
     return list_of_files
+
+# Take a string 'dir/of/file' and make tuple ('dir/of', 'file')
+def makeTuple(s):
+    s = s.strip()
+    i = s.rindex('/') if s[-1] != '/' else s[:-1].rindex('/')
+    return (s[:i], s[i+1:])
 
 # Make the name of the archive. IT will be the word backmeup plus a date
 def getFileName():
@@ -43,9 +60,5 @@ def getFileName():
 
 # Main
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print 'Usage: ./backmeup'
-        exit()
-
     Compress()
 
