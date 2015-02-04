a6736c8893a1fc762f7c239298c6ea6d5721e9e5
petermlm <pedromelgueira@gmail.com>
2015-02-04 17:19:12 +0000

    PEP8 and other small changes

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ BackUp is a small script to help me create quick and dirty tar file containing b
 
 I developed this script because I had a few directories in separate locations and I wanted to back them up into a single tar file.
 
+No really... I just wanted to try GitHub without bothering anyone. But I still use the script.
+
 # How To Use
 
 BackUp works by reading from a file, called bu\_files, which contain the files and directories you want to store to the target tar file.
@@ -20,7 +22,7 @@ In the above example you have two directories in the home folder of someuser, on
 
 By local, it's meant that the file/directory being mention is in the same directory has the BackUp script.
 
-Note that has of this version, tiled (~) can't be use to refer to the home directory.
+Note that as of this version, tiled (~) can't be use to refer to the home directory.
 
 The contents of the tar file will be the following.
 
@@ -38,4 +40,3 @@ The location of each file/directory is not preserve in the tar file. Because of 
     /dir2/file
 
 The script warns the user about this.
-
--- backup.py
+++ backup.py
@@ -7,9 +7,11 @@ import datetime
 
 """ Compress """
 
+
 # Compress to the backup file
 def Compress(bu_files, target_file_name):
-    # Get list of files and directories to compress. List will have tuples like:
+    # Get list of files and directories to compress. List will have tuples
+    # like:
     # (real directory, object, dir in tar)
     to_compress = getFilesToCompress(bu_files)
 
@@ -26,6 +28,7 @@ def Compress(bu_files, target_file_name):
             os.system(cmd)
         else:
             print "File " + i[0] + "/" + i[1] + " does not exist. Skipping."
+
 
 # Get files and directories from the file bu_files
 def getFilesToCompress(bu_files):
@@ -54,13 +57,16 @@ def getFilesToCompress(bu_files):
 
     return list_of_files
 
+
 # Take a string "dir/of/file" and make tuple ("dir/of", "file")
 def makeTuple(s):
     s = s.strip()
     i = s.rindex("/") if s[-1] != "/" else s[:-1].rindex("/")
     return (s[:i], s[i+1:])
 
+
 """ Exception for Argument Parsing """
+
 
 class BadArgs(Exception):
     def __init__(self, text):
@@ -69,7 +75,9 @@ class BadArgs(Exception):
     def __str__(self):
         return self.text
 
+
 """ Parse the Arguments """
+
 
 def parseArgs(argv):
     bu_files = ""
@@ -94,7 +102,9 @@ def parseArgs(argv):
 
     return bu_files, target_file_name
 
+
 """ Get the Directory and the File Name """
+
 
 class NotDirOrFile(Exception):
     def __init__(self, text):
@@ -103,11 +113,13 @@ class NotDirOrFile(Exception):
     def __str__(self):
         return "Exception: " + self.text + " is neither a file nor directory."
 
+
 # Make the name of the archive. IT will be the word backup plus a date
 def getDefaultFileName():
     today = datetime.date.today()
     date_str = str(today.year) + "_" + str(today.month) + "_" + str(today.day)
     return "backup_" + date_str + ".tar"
+
 
 def getDirAndFileName(target_file_name):
     if target_file_name == "":
@@ -159,4 +171,3 @@ if __name__ == "__main__":
 
     # Compress
     Compress(bu_files, target_file_name)
-
