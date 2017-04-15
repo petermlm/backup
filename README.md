00837212cdca20216911bad839a818ec6a204d7c
Pedro Melgueira <pedromelgueira@gmail.com>
2017-04-15 01:46:47 +0100

    Reimplements script in Python 3

--- README.md
+++ README.md
@@ -1,51 +1,43 @@
 # About
 
-BackUp is a small script to help me create quick and dirty tar file containing backups of my file system.
+BackUp is a small script to help me create quick and dirty tar file containing
+backups of my file system.
 
-I developed this script because I had a few directories in separate locations and I wanted to back them up into a single tar file.
+I developed this script because I had a few directories in separate locations
+and I wanted to back them up into a single tar file.
 
-No really... I just wanted to try GitHub without bothering anyone. But I still use the script.
-
-(Disclaimer: The author is not responsible for any problems you with encounter with the usage of this project.)
+Also because back in 2013 I was new to Git and GitHub and wanted to try these
+things.
 
 # Arguments
 
-    -h             - Display a... somewhat helpful help text.
-    -b bu_file     - bu_file location. If this flag is not specified, the
-                     default location where the script will look for the
-                     bu_file is the same as the execution directory.
-    -o target_file - The resulting tar file. Default is stored in the same
-                     directory with a default name.
+    -h             - Display help
+    -b bu_file     - bu_file location. Defaults to the script's directory
+    -o target_file - The resulting tar file. Default to the script's directory
 
 # How To Use
 
-BackUp works by reading from a file, called bu\_files, which contain the files and directories you want to store to the target tar file.
+BackUp works by reading from a file, called `bu_files`, which contain the files
+and directories you want to store to the target tar file. The directories may
+be absolute or relative.
 
-Just write the directory locations to the bu\_file and you're good to go.
+For example, if `bu_files` has:
 
     /home/someuser/Documents/
     /home/someuser/Images/
-    /var/www/web_things/
     local_file
     local_directory
 
-In the above example you have two directories in the home folder of someuser, one directory in /var/www, a local file and a local directory.
-
-By local, it's meant that the file/directory being mention is in the same directory has the BackUp script.
-
-Note that as of this version, tiled (~) can't be use to refer to the home directory.
-
-The contents of the tar file will be the following.
+The contents of the tar file will be:
 
     backup_2013_06_23.tar
-    |--bu_files
     |--Documents/
     |--Images/
-    |--localfile
+    |--local_file
     |--local_directory
-    |--web_things/
 
-The location of each file/directory is not preserve in the tar file. Because of that there can't be two files with the same name, like for example:
+The location of each file/directory is not preserve in the tar file. Because of
+that there can't be two files with the same name, like for example:
 
     /dir1/file
     /dir2/file
--- backup.py
+++ backup.py
@@ -1,173 +1,162 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
+
+# TODO: If the target file is given, the name should be changed
 
 # System imports
 import os
 import sys
 import datetime
 
-""" Compress """
+
+# =============================================================================
+# Create archive
+# =============================================================================
+
+
+def addFile(directory, target_file_name, file_to_add):
+    os.chdir(directory)
+    cmd = "tar -r --file=" + target_file_name + " " + file_to_add
+    os.system(cmd)
 
 
 # Compress to the backup file
 def Compress(bu_files, target_file_name):
     # Get list of files and directories to compress. List will have tuples
     # like:
-    # (real directory, object, dir in tar)
+    # (directory, filename)
     to_compress = getFilesToCompress(bu_files)
 
-    # Make tar with bu_files
-    cmd = "tar -cf " + target_file_name + " " + bu_files
-    os.system(cmd)
+    # Make tar with bu_files (Not needed for now)
+    # os.chdir(os.path.dirname(bu_files))
+    # cmd = "tar -cf " + target_file_name + " " + bu_files
+    # os.system(cmd)
 
     # Add files in to_compress
     for i in to_compress:
-        if os.path.exists(i[0] + "/" + i[1]):
-            print "Adding file " + i[0] + "/" + i[1]
-            os.chdir(i[0])
-            cmd = "tar -r --file=" + target_file_name + " " + i[1]
-            os.system(cmd)
+        file_path = os.path.join(i[0], i[1])
+
+        if os.path.exists(file_path):
+            print("Adding file \"{}\"".format(file_path))
+            addFile(i[0], target_file_name, i[1])
+
         else:
-            print "File " + i[0] + "/" + i[1] + " does not exist. Skipping."
+            print("File \"{}\" does not exist. Skipping.".format(file_path))
 
 
 # Get files and directories from the file bu_files
 def getFilesToCompress(bu_files):
-    to_compress = open(bu_files)
+    bu_files_fd = open(bu_files)
     list_of_files = []
 
-    for i in to_compress:
-        # Make tuple to append
-        if i != "\n":
-            if i[0] != "/":
-                app = os.getcwd() + "/"
-            else:
-                app = ""
+    for i in bu_files_fd:
+        line = i.strip()
 
-            t = makeTuple(app + i)
+        if line == "":
+            continue
 
-            # Check name collision
-            for j in list_of_files:
-                if j[1] == t[1]:
-                    print j, t
-                    print "Warning! Name collision: " + t[1]
-                    exit()
+        if line[-1] == "/":
+            line = line[:-1]
 
-            # Append
-            list_of_files.append(t)
+        if line[0] != "/":
+            line = os.path.join(os.getcwd(), line)
+
+        entry = (os.path.dirname(line), os.path.basename(line))
+
+        # Check name collision
+        for j in list_of_files:
+            # if j[1] == entry[1] or j[1] == bu_files: (bu_files not needed in
+            # archive for now)
+            if j[1] == entry[1]:
+                print("Multiple files with name \"{}\"".format(j[1]))
+                exit(1)
+
+        # Append
+        list_of_files.append(entry)
 
     return list_of_files
 
 
-# Take a string "dir/of/file" and make tuple ("dir/of", "file")
-def makeTuple(s):
-    s = s.strip()
-    i = s.rindex("/") if s[-1] != "/" else s[:-1].rindex("/")
-    return (s[:i], s[i+1:])
+# =============================================================================
+# Script Arguments
+# =============================================================================
 
 
-""" Exception for Argument Parsing """
+def printUsage():
+    print("Usage:")
+    print("\t-h             - Display help")
+    print("\t-b bu_file     - bu_file location. Defaults to the script's directory")
+    print("\t-o target_file - The resulting tar file. Default to the script's directory")
 
 
-class BadArgs(Exception):
-    def __init__(self, text):
-        self.text = text
-
-    def __str__(self):
-        return self.text
-
-
-""" Parse the Arguments """
+def getDefaultFileName():
+    today = datetime.date.today()
+    date_str = str(today.year) + "_" + str(today.month) + "_" + str(today.day)
+    return os.path.join(os.getcwd(), "backup_" + date_str + ".tar")
 
 
 def parseArgs(argv):
-    bu_files = ""
-    target_file_name = ""
+    bu_files = os.path.join(os.getcwd(), "bu_files")
+    target_file_name = getDefaultFileName()
 
-    if len(argv) > 1:
-        # If not, parse the arguments
-        for i in range(1, len(argv), 2):
-            if argv[i] == "-b":
-                if "-b" in argv[i+1:]:
-                    raise BadArgs("More then one -b flag. Quitting.")
+    # Get file names from arguments
+    i = 0
+    while i < len(argv):
+        if argv[i] == "-b":
+            try:
                 bu_files = argv[i+1]
+            except IndexError:
+                print("Flag -b needs argument")
+                print("")
+                printUsage()
+                exit(1)
+            i += 2
 
-            elif argv[i] == "-o":
-                if "-o" in argv[i+1:]:
-                    raise BadArgs("More then one -o flag. Quitting.")
-
+        elif argv[i] == "-o":
+            try:
                 target_file_name = argv[i+1]
 
-            else:
-                raise BadArgs("Argument \"" + argv[i] + "\" not recognized")
+                if not os.path.isabs(target_file_name):
+                    target_file_name = os.path.join(os.getcwd(), target_file_name)
+
+                if os.path.splitext(target_file_name)[1] != ".tar":
+                    target_file_name += ".tar"
+
+            except IndexError:
+                print("Flag -o needs argument")
+                printUsage()
+                print("")
+                exit(1)
+            i += 2
+
+        else:
+            print("Argument {} not recognized".format(argv[i]))
+            print("")
+            printUsage()
+            exit(1)
 
     return bu_files, target_file_name
 
 
-""" Get the Directory and the File Name """
+# =============================================================================
+# Main
+# =============================================================================
 
-
-class NotDirOrFile(Exception):
-    def __init__(self, text):
-        self.text = text
-
-    def __str__(self):
-        return "Exception: " + self.text + " is neither a file nor directory."
-
-
-# Make the name of the archive. IT will be the word backup plus a date
-def getDefaultFileName():
-    today = datetime.date.today()
-    date_str = str(today.year) + "_" + str(today.month) + "_" + str(today.day)
-    return "backup_" + date_str + ".tar"
-
-
-def getDirAndFileName(target_file_name):
-    if target_file_name == "":
-        return getDefaultFileName()
-
-    elif os.path.isfile(target_file_name):
-        return target_file_name
-
-    elif os.path.isdir(target_file_name):
-        return os.path.join(target_file_name, getDefaultFileName())
-
-    else:
-        dir_name = os.path.split(target_file_name)[0]
-        if os.path.isdir(dir_name):
-            return target_file_name
-
-    raise NotDirOrFile(target_file_name)
-
-""" Main """
 
 if __name__ == "__main__":
     # Take arguments
     # If there is a help flag, just display the help and quit.
     if "-h" in sys.argv:
-        print "Check the README.md file."
-        exit()
+        printUsage()
+        exit(1)
 
-    try:
-        bu_files, target_file_name = parseArgs(sys.argv)
+    bu_files, target_file_name = parseArgs(sys.argv[1:])
 
-    except IndexError:
-        print "Error in arguments. No tar file created."
-        exit()
+    if not os.path.isfile(bu_files):
+        print("File {} doesn't exist".format(bu_files))
+        exit(1)
 
-    except BadArgs as ba:
-        print ba
-        exit()
+    if os.path.isfile(target_file_name):
+        print("File {} already exists".format(target_file_name))
+        exit(1)
 
-    # If the bu_files name is empty, set it to the default name
-    if bu_files == "":
-        bu_files = "bu_files"
-
-    # Get the directory and the file name
-    try:
-        target_file_name = getDirAndFileName(target_file_name)
-    except NotDirOrFile as e:
-        print e
-        exit()
-
-    # Compress
     Compress(bu_files, target_file_name)
